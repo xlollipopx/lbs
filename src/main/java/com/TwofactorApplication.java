0069139f0d722a65432f5092315955fa4063580b
anton <astelmakh@linxup.com>
2022-09-18 13:07:46 +0300

    init commit

---
+++ src/main/java/com/TwofactorApplication.java
@@ -0,0 +1,21 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		SecurityAutoConfiguration.class
+})
+public class TwofactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwofactorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    ResponseEntity<?> hello() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Hello");
+    }
+}
